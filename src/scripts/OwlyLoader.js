8eddda2bff7e802c7f6a1fed37e44b35f8ccbd50
jciasenza <jciasenza@aulasneo.com>
2025-08-05 10:32:21 -0300

    Fix OwlyLoader

--- src/scripts/OwlyLoader.js
+++ src/scripts/OwlyLoader.js
@@ -1,53 +1,30 @@
+/**
+ * @implements {OwlyLoader}
+ * @memberof module:Owly
+ */
 class OwlyLoader {
-  constructor() {
-    this.s3BaseUrl = 'https://chat.owly.aulasneo.com/';
-    this.containerId = 'owly-react-container';
-    this.loaded = false;
-  }
+  loadScript() {
+    global.owly = global.owly || [];
+    const { owly } = global;
 
-  load() {
-    if (this.loaded) {
-      console.warn('La aplicación ya fue cargada');
+    if (owly.invoked) {
       return;
     }
 
-    try {
-      this.createContainer();
-      this.embedIframe();
-      this.loaded = true;
-    } catch (error) {
-      console.error('Error al cargar la aplicación:', error);
-    }
-  }
+    owly.invoked = true;
 
-  createContainer() {
-    let container = document.getElementById(this.containerId);
-    if (!container) {
-      container = document.createElement('div');
-      container.id = this.containerId;
-      container.style.width = '100%';
-      container.style.height = '100vh';
-      container.style.border = 'none';
-      document.body.appendChild(container);
-    }
-  }
+    owly.load = (key, options) => {
+      const scriptSrc = document.createElement('script');
+      scriptSrc.type = 'text/javascript';
+      scriptSrc.async = true;
+      scriptSrc.src = 'https://chat.owly.aulasneo.com/';
+      const first = document.getElementsByTagName('script')[0];
+      first.parentNode.insertBefore(scriptSrc, first);
 
-  embedIframe() {
-    const container = document.getElementById(this.containerId);
-    if (!container) {
-      console.error('El contenedor no existe. ¿Olvidaste llamar a createContainer()?');
-      return;
-    }
-    container.innerHTML = '';
-    const iframe = document.createElement('iframe');
-    iframe.src = this.s3BaseUrl;
-    iframe.style.width = '100%';
-    iframe.style.height = '100%';
-    iframe.style.border = 'none';
-    iframe.allowFullscreen = true;
-    iframe.loading = 'lazy';
-    iframe.title = 'Owly Chat';
-    container.appendChild(iframe);
+      owly._loadOptions = options;
+    };
+
+    owly.load();
   }
 }
 
